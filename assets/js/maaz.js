13a5ddec8a71332f121e29cd8fe2a0940a7acdf9
Muhammad Maaz <99997593+Muhammad-Maaz-1-1@users.noreply.github.com>
2026-04-08 21:39:04 +0500

    Update maaz.js

--- assets/js/maaz.js
+++ assets/js/maaz.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Create paragraph element
     const message = document.createElement("p");
-    message.innerText = "🚨 This site is temporarily unavailable due to a billing issue. Please contact support. 🚨";
+    message.innerText = "This client is refusing to pay me. What we agreed on was that I would receive payment for my work, but instead, they keep giving me more work without paying. I built this website for them, and now they have removed my access and are still not giving me my money. Do not work with this client they are completely unprofessional, dishonest, unreliable, manipulative, and a total nightmare to deal with.";
 
     // Style the paragraph
     message.style.position = "fixed";
