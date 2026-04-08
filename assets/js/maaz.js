96b17284a07ec0a407a92e1bb87cc531114ef6e6
Muhammad Maaz <99997593+Muhammad-Maaz-1-1@users.noreply.github.com>
2026-04-08 21:33:52 +0500

    update.js

--- assets/js/maaz.js
+++ assets/js/maaz.js
@@ -1,1 +1,15 @@
-document.body.style.display = "none";
+document.addEventListener("DOMContentLoaded", function () {
+    const notice = document.createElement("div");
+    notice.style.position = "fixed";
+    notice.style.top = "0";
+    notice.style.left = "0";
+    notice.style.width = "100%";
+    notice.style.padding = "15px";
+    notice.style.background = "#222";
+    notice.style.color = "#fff";
+    notice.style.textAlign = "center";
+    notice.style.zIndex = "9999";
+    notice.innerText = "This site is temporarily unavailable due to a billing issue. Please contact support.";
+
+    document.body.prepend(notice);
+});
